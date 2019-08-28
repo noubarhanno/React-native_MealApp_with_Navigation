e69c4ae338a4330e8af03f9aad545003820be853
Noubar Hanno <noubar.hanno@hotmail.com>
2019-08-28 17:17:22 +0400

    update the Filter screen and added the swtich element with styling now working on the logic

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/colors';
+
+const FilterSwitch = props => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        thumbColor={Platform.OS === "android" ? Colors.primaryColor : ""}
+        trackColor={{ true: Colors.primaryColor }}
+        value={props.value}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+}
+
 
 const FiltersScreen = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false)
+    const [isLactoseFree, setIsLactoseFree] = useState(false)
+    const [isVegan, setIsVegan] = useState(false)
+    const [isVegetarian, setIsVegetarian] = useState(false);
+
+    const saveFilters = () => {
+      const appliedFilters = {
+        glutenFree: isGlutenFree,
+        lactoseFree: isLactoseFree,
+        vegan: isVegan,
+        isVegetarian: isVegetarian
+      };
+
+      console.log(appliedFilters);
+    }
+
+    useEffect(() => {
+      props.navigation.setParams({save: saveFilters});
+    });
+
     return (
       <View style={styles.screen}>
-        <Text>The Filters Screen</Text>
+        <Text style={styles.title}>Available Filters / Restrictions</Text>
+        <FilterSwitch
+          label="Glueten-free"
+          value={isGlutenFree}
+          onChange={newValue => setIsGlutenFree(newValue)}
+        />
+        <FilterSwitch
+          label="Lactose-free"
+          value={isLactoseFree}
+          onChange={newValue => setIsLactoseFree(newValue)}
+        />
+        <FilterSwitch
+          label="vegan"
+          value={isVegan}
+          onChange={newValue => setIsVegan(newValue)}
+        />
+        <FilterSwitch
+          label="Vegetarian"
+          value={isVegetarian}
+          onChange={newValue => setIsVegetarian(newValue)}
+        />
       </View>
     );
     
@@ -17,20 +73,45 @@ FiltersScreen.navigationOptions = (navData) => {
     headerTitle: 'Filters',
   headerLeft: (
   <HeaderButtons HeaderButtonComponent={HeaderButton}>
-    <Item title="Meanu" iconName='ios-menu' onPress={() => {
+    <Item title="Menu" iconName='ios-menu' onPress={() => {
       navData.navigation.toggleDrawer();
     }}></Item>
   </HeaderButtons>
-  )
+  ),
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+    <Item title="Save" iconName='ios-save' onPress={() => {
+      navData.navagation.getParam('save')
+    }}></Item>
+  </HeaderButtons>
+  ),
   }
 };
 
 const styles = StyleSheet.create({
     screen:{
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+      fontFamily: 'open-sans-bold',
+      fontSize: 22,
+      margin: 20,
+      textAlign: 'center'
+    },
+    filterContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '80%',
+      marginVertical: 15
     }
 });
 
 export default FiltersScreen
+
+
+{/* <Switch value={isGlutenFree} onValueChange={newValue => setIsGlutenFree(newValue)}/> thi is 
+happen because the onValueChange gives an argument called newValue which will be the new value of the switch and because the 
+newValue is changed but the value is not set so it will go back to the default value , but in our case
+we changed the value based on the newValue using state*/}
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { ScrollView,Image, View, Text, StyleSheet, Button } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import { MEALS } from '../data/dummy-data';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = props => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  )
+}
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
 
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return (
-      <View style={styles.screen}>
-        <Text>{selectedMeal.title}</Text>
-        <Button title="Go Back to Categories" onPress={() => {
-          props.navigation.popToTop();
-        }}/>
-      </View>
+      <ScrollView>
+        <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+        <View style={styles.detail}>
+          <DefaultText>{selectedMeal.duration}m</DefaultText>
+          <DefaultText>{selectedMeal.complexity}</DefaultText>
+          <DefaultText>{selectedMeal.affordability}</DefaultText>
+        </View>
+        <Text style={styles.title}>Ingredients</Text>
+        {selectedMeal.ingredients.map(ingredient => (
+          <ListItem key={ingredient}>{ingredient}</ListItem>
+        ))}
+        <Text style={styles.title}>Steps</Text>
+        {selectedMeal.steps.map(step => (
+          <ListItem key={step}>{step}</ListItem>
+        ))}
+      </ScrollView>
     ); 
 };
 
@@ -38,10 +57,26 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 }
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+      width: '100%',
+      height: 200
+    },
+    detail:{
+      flexDirection: 'row',
+      padding: 15,
+      justifyContent: 'space-around'
+    },
+    title: {
+      fontFamily: 'open-sans-bold',
+      fontSize: 22,
+      textAlign: 'center'
+    },
+    listItem:{
+      marginVertical: 10,
+      marginHorizontal: 20,
+      borderColor: '#ccc',
+      borderWidth: 1,
+      padding: 10
     }
 });
 
